ec49e7bf884d825d557c49065a28edd0d408133c
AlwayXX <107671942+AlwayXX@users.noreply.github.com>
2022-06-19 16:57:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function hello(text1,text2){
+   // document.write("haha" + text1 + "OK" + text2);
+    return text1 + text2;
+}
+
+hello("araara",123);
+document.write(hello("araara",123));
+
+        
