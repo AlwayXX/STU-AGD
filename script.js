71f2c792fbce8eac307385fe2ac767e69d96e622
AlwayXX <107671942+AlwayXX@users.noreply.github.com>
2022-06-19 16:56:51 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function hello(text1,text2){
-   // document.write("haha" + text1 + "OK" + text2);
-    return text1 + text2;
-}
-
-hello("araara",123);
-document.write(hello("araara",123));
-
-        
